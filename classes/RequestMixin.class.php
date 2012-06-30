db4a1fc3253cd2484fa29012c0b1bd72b2c31903
Ben Allfree <ben@softwareato.com>
2012-06-30 11:42:53 -0700

    Cleanup

--- classes/RequestMixin.class.php
+++ classes/RequestMixin.class.php
@@ -2,7 +2,52 @@
 
 class RequestMixin extends Mixin
 {
-  function p($name, $val='')
+  static $request = array();  
+  static function init()
+  {
+    $path = substr($_SERVER['REQUEST_URI'], strlen('/'));
+    $parts = explode('?', $path);
+    $full_request_path = trim($_SERVER['REQUEST_URI'],"/");
+    $protocol = isset($_SERVER['HTTPS']) ? 'https' : 'http';
+    $current_url = "{$protocol}://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+    $request_path = vpath("/{$parts[0]}");
+    $params = array_merge($_GET, $_POST);
+    
+    // Fix file struct
+    $fields = array('name', 'type', 'tmp_name', 'error', 'size');
+    foreach($_FILES as $k=>$v) {
+      foreach($fields as $field)
+      {
+        if (count($v['name'])==0) break;
+        merge_bottom($params[$k], $v[$field], $field);
+      }
+    }
+    
+    $host = $_SERVER['HTTP_HOST'];
+    $parts = explode('.', $host);
+    
+    $domain = join('.', array_slice($parts, -2, 2));
+    
+    $subdomain = join('.', array_slice($parts, 0, count($parts)-2));
+    
+    $querystring = $_SERVER['QUERY_STRING'];
+    
+    if (strpos($subdomain, '_')) trigger_error("Subdomains with _ are not supported. They break sessions in IE7/8, possibly others.", E_USER_ERROR);
+    
+    $request = W::filter('parse_request', array(
+      'domain'=>$domain,
+      'subdomain'=>$subdomain,
+      'host'=>$host,
+      'querystring'=>$querystring,
+      'path'=>$request_path,
+      'params'=>$params,
+      'current_url'=>$current_url,
+      'protocol'=>$protocol,
+    ));
+    self::$request = $request;  
+  }
+  
+  static function p($name, $val='')
   {
     $elems = preg_split("/[\[\]]/", $name);
     $name = '';
@@ -11,13 +56,12 @@ class RequestMixin extends Mixin
       if(!$e) continue;
       $name.= "['$e']";
     }
-    global $__wicked;
-    $p = $__wicked['modules']['request']['request'];
+    $p = self::$request;
     if (eval("return isset(\$p['params']$name);")) return eval("return \$p['params']$name;");
     return $val;
   }
   
-  function q($s, $default='')
+  static function q($s, $default='')
   {
     if (!array_key_exists($s,$_REQUEST)) return $default;
     return $_REQUEST[$s];
